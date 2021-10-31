be8eafee4f7453574e57ffc416d2c594ec99bcf0
derrick <derrickethornton@protonmail.com>
2021-10-30 23:51:56 -0500

    Wrote a new page for my website: the about section.

---
+++ src/pages/about.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <p>Hello, I'm Derrick, a software developer.
+                I specialize in planning, designing and developing
+                software products. Currently, I'm intensely focused 
+                on fleshing out my portfolio, so if you need something
+                done: I'm your guy. 
+            </p>
+            <p>As I've worked with a variety of tools and frameworks,
+                relevant to web development, I feel as if I'm capable
+                of fulfilling whatever requirements for the web you need.
+                
+            </p>
+        </main>
+    )
+}
