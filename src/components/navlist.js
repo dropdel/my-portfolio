cd65643846fb744977265be4806a615ee172e123
derrick <derrickethornton@protonmail.com>
2021-11-01 07:36:48 -0500

    I removed the default padding and margin from the root html and body elements

--- src/components/navlist.js
+++ src/components/navlist.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import { nav, listItem } from './navlist.module.css'
+import * as styles from './navlist.module.css'
 /* 
     Navigation List for use in the header.
 */
@@ -14,12 +14,12 @@ const NavList = (props) => {
         (pathAndText) => {
             return (
             <li>
-                <Link to={pathAndText[0]} className={listItem}>{pathAndText[1]}</Link>
+                <Link to={pathAndText[0]} className={styles.listItem}>{pathAndText[1]}</Link>
             </li>
             )
         }
     )
-    return (<ul className={nav}>{listItems}</ul>)
+    return (<ul className={styles.nav}>{listItems}</ul>)
 }
 
 export default NavList;
