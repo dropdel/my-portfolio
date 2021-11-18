45a0dcc86ca7f9948a5f78b243df6a0f3762f036
derrick <derrickethornton@protonmail.com>
2021-11-18 08:40:48 -0600

    Removed gatsby source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,12 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Portfolio",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'My Portfolio',
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
+    'gatsby-plugin-image',
+    'gatsby-plugin-mdx',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
 };
